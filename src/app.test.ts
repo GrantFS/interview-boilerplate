af267fd057799b2d5c96c3d63fb72ff6c6d332f4
Michael Patricios <michael.patricios@gmail.com>
2020-12-04 10:13:30 +0000

    Kata complete?

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,16 +1,28 @@
 import { fizzBuzz } from "./app";
 describe('first test', () => {
     it('returns a string for the number', () => {
-
         expect(fizzBuzz(1)).toBe('1');
         expect(fizzBuzz(2)).toBe('2');
         expect(fizzBuzz(4)).toBe('4');
         expect(fizzBuzz(98)).toBe('98');
     });
-    it('returns fizz multiple of 3', () => {
-        expect(fizzBuzz(3)).toBe('fizz');
-        expect(fizzBuzz(6)).toBe('fizz');
-        expect(fizzBuzz(99)).toBe('fizz');
+
+    it('returns Fizz for a multiple of 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+        expect(fizzBuzz(6)).toBe('Fizz');
+        expect(fizzBuzz(99)).toBe('Fizz');
+    });
+
+    it('returns Buzz for a multiple of 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
+        expect(fizzBuzz(10)).toBe('Buzz');
+        expect(fizzBuzz(100)).toBe('Buzz');
+    });
+
+    it('returns FizzBuzz for a multiple of 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+        expect(fizzBuzz(30)).toBe('FizzBuzz');
+        expect(fizzBuzz(90)).toBe('FizzBuzz');
     });
 });
 
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 export function fizzBuzz(n:number): string {
-    if(n % 3 === 0) {
-        return 'fizz';
+    if (n % 3 === 0 && n % 5 === 0) {
+        return 'FizzBuzz';
+    } else if (n % 3 === 0) {
+        return 'Fizz';
+    } else if (n % 5 === 0) {
+        return 'Buzz';
     }
     return ''+n;
 
