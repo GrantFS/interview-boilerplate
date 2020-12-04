e5e411fe3d7c73871e8b5620df2387904cfd3a46
Grant Pearce <grant.pearce@cazoo.co.uk>
2020-12-04 10:07:35 +0000

    add test for 3

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,16 @@
 import { fizzBuzz } from "./app";
 describe('first test', () => {
-    it('', () => {
+    it('returns a string for the number', () => {
+
         expect(fizzBuzz(1)).toBe('1');
+        expect(fizzBuzz(2)).toBe('2');
+        expect(fizzBuzz(4)).toBe('4');
+        expect(fizzBuzz(98)).toBe('98');
+    });
+    it('returns fizz multiple of 3', () => {
+        expect(fizzBuzz(3)).toBe('fizz');
+        expect(fizzBuzz(6)).toBe('fizz');
+        expect(fizzBuzz(99)).toBe('fizz');
     });
 });
+
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
 export function fizzBuzz(n:number): string {
-    return '1';
+    if(n % 3 === 0) {
+        return 'fizz';
+    }
+    return ''+n;
+
 }
