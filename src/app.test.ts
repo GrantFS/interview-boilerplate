33c3143d4a0011575e6789d3109709d61af009d6
Vittorio Guerriero <vittorio.gue@gmail.com>
2020-12-11 10:18:24 +0000

    add another test

--- src/app.test.ts
+++ src/app.test.ts
@@ -3,5 +3,9 @@ describe('first test', () => {
     it('returns a string for the number', () => {
         expect(fizzBuzz(1)).toBe('1');
     });
+
+    it('returns 2 as a string for the number 2', () => {
+        expect(fizzBuzz(2)).toBe('2');
+    });
 });
 
