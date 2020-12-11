19b2fa511f404cf4d1a398375f3efd8baddd9091
Grant Pearce <grant.pearce@cazoo.co.uk>
2020-12-11 10:21:48 +0000

    pass the 2nd test and add a third

--- src/app.test.ts
+++ src/app.test.ts
@@ -7,5 +7,9 @@ describe('first test', () => {
     it('returns 2 as a string for the number 2', () => {
         expect(fizzBuzz(2)).toBe('2');
     });
+
+    it('return Fizz when the number 3 is processed', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+    });
 });
 
