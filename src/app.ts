ae6ecc2a7b6962ad55222bdb7f5c2902632a7b6f
Vittorio Guerriero <vittorio.gue@gmail.com>
2020-12-11 10:40:53 +0000

    make it pass number 10

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ export function fizzBuzz(n:number): string {
     if (n % 3 === 0) {
         return 'Fizz';
     }
-    if(n === 5) {
+    if (n === 5 || n === 10) {
         return 'Buzz';
     }
     return ''+n;
