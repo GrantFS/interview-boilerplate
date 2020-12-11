3d060170a77c4fc1840dad66fb3883411f24c4e2
Grant Pearce <grant.pearce@cazoo.co.uk>
2020-12-11 10:15:11 +0000

    add first test

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,9 +2,6 @@ import { fizzBuzz } from "./app";
 describe('first test', () => {
     it('returns a string for the number', () => {
         expect(fizzBuzz(1)).toBe('1');
-        expect(fizzBuzz(2)).toBe('2');
-        expect(fizzBuzz(4)).toBe('4');
-        expect(fizzBuzz(98)).toBe('98');
     });
 });
 
