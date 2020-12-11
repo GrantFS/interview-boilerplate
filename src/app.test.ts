8bfef599454997efb5468c922b89fe858c424faa
Grant Pearce <grant.pearce@cazoo.co.uk>
2020-12-11 10:02:39 +0000

    init

--- src/app.test.ts
+++ src/app.test.ts
@@ -6,23 +6,5 @@ describe('first test', () => {
         expect(fizzBuzz(4)).toBe('4');
         expect(fizzBuzz(98)).toBe('98');
     });
-
-    it('returns Fizz for a multiple of 3', () => {
-        expect(fizzBuzz(3)).toBe('Fizz');
-        expect(fizzBuzz(6)).toBe('Fizz');
-        expect(fizzBuzz(99)).toBe('Fizz');
-    });
-
-    it('returns Buzz for a multiple of 5', () => {
-        expect(fizzBuzz(5)).toBe('Buzz');
-        expect(fizzBuzz(10)).toBe('Buzz');
-        expect(fizzBuzz(100)).toBe('Buzz');
-    });
-
-    it('returns FizzBuzz for a multiple of 3 and 5', () => {
-        expect(fizzBuzz(15)).toBe('FizzBuzz');
-        expect(fizzBuzz(30)).toBe('FizzBuzz');
-        expect(fizzBuzz(90)).toBe('FizzBuzz');
-    });
 });
 
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,3 @@
 export function fizzBuzz(n:number): string {
-    let result = '';
 
-    if (n % 3 === 0) {
-        result += 'Fizz';
-    }
-    if (n % 5 === 0) {
-        result += 'Buzz';
-    }
-    return result == '' ? ''+n : result;
 }
