fb8cc4b9fbde36c217c193facbe9863d9ece451d
Vittorio Guerriero <vittorio.gue@gmail.com>
2020-12-11 11:19:26 +0000

    first test for isLeapYearh

---
+++ src/leapYear.test.ts
@@ -0,0 +1,9 @@
+function isLeapYear(s: string): boolean {
+    return undefined;
+}
+
+describe('leapYear', () => {
+    it('isLeapYear', () => {
+        expect(isLeapYear('1996')).toBe(true);
+    });
+});
