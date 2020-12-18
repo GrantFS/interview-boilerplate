e025f02f56b58596faf7b24dccdf606daa605fea
Vittorio Guerriero <vittorio.guerriero@cazoo.co.uk>
2020-12-18 09:46:00 +0000

    🔴🔴🔴🔴 RED -  - to squash

--- src/calc.test.js
+++ src/calc.test.js
@@ -2,6 +2,10 @@ describe('Simple Calculator', () => {
     it('accept a string of numbers', () => {
         expect(Add('1')).toBe(1);
     });
+
+    it('accept a string of numbers', () => {
+        expect(Add('2')).toBe(2);
+    });
 });
 
 function Add(numbers) {
